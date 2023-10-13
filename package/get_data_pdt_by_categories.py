00072e66cd9bffcf986ae066ab3de13eda782d1d
augustinverdier <aug.verdier@gmail.com>
2023-10-13 12:04:10 +0200

    fonction terminée mais pas fonctionelle hahahahaha

--- package/get_data_pdt_by_categories.py
+++ package/get_data_pdt_by_categories.py
@@ -22,17 +22,16 @@ def get_books_from_categories(url):
     books = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
     for book in books:
-        books_link = book.find("a", href_="")
-
-    chemin = r"/Users/augustinverdier/PycharmProjects/projet_2/liste_livres.txt"
-    with open(chemin, "a",) as f:
-        f.write(f"{books_link}")
+        books_link = book.find_all_next("a", href_="")
+        return books_link
 
 
-"""
 def get_books_from_all_page_by_categories():
+
     pages = get_all_categories_pages()
     for page in pages:
         get_books_from_categories(url=page)
-        print(f"on scrap + {page}")
-"""
+        return page
+
+
+print(get_books_from_all_page_by_categories())
