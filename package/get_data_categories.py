1394ff3efe641ccd6297ff8fffaa832d3ebac4f2
augustinverdier <aug.verdier@gmail.com>
2023-10-13 17:22:14 +0200

    création de la fonction get_data_categories_fct() mais elle ne fonctionne pas

--- package/get_data_categories.py
+++ package/get_data_categories.py
@@ -6,13 +6,17 @@ def get_data_categories_fct():
 
     r = requests.get("http://books.toscrape.com/index.html")
     soup = BeautifulSoup(r.content, "html.parser")
-    categories_link = []
-    categories_name = []
 
     navbar = soup.find("ul", class_="nav nav-list")
-    categories_list = navbar.find_all("a", href_="")
+    for genre in navbar:
+        categories_link = genre.find_all("li")
+        for nom in categories_link:
+            categories_name = nom.find_all("#text")
+        print(categories_link)
+"""
+    for genre in categories_list:
+        categories_link.append("li")
 
-    print(categories_list)
+        print(categories_link)
+"""
 
-
-get_data_categories_fct()
