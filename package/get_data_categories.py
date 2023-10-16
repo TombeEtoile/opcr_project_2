559529b1c1152556a39ebd4bfc887dfb84b76034
augustinverdier <aug.verdier@gmail.com>
2023-10-16 17:09:10 +0200

    bloqué pour la récupération de la liste d'url (j'arrive pas à enlever le code html)

--- package/get_data_categories.py
+++ package/get_data_categories.py
@@ -7,16 +7,14 @@ def get_data_categories_fct():
     r = requests.get("http://books.toscrape.com/index.html")
     soup = BeautifulSoup(r.content, "html.parser")
 
-    navbar = soup.find("ul", class_="nav nav-list")
-    for genre in navbar:
-        categories_link = genre.find_all("li")
-        for nom in categories_link:
-            categories_name = nom.find_all("#text")
-        print(categories_link)
-"""
-    for genre in categories_list:
-        categories_link.append("li")
+    categories = []
 
-        print(categories_link)
-"""
+    navbar = soup.find("div", class_="side_categories")
+    urls = navbar.find_all("a")
+    for link in urls:
+        href = link['href']
 
+        return href
+
+
+print(get_data_categories_fct())
