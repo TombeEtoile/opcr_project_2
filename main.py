656676443a62a40577c89c088728c28415b983d4
augustinverdier <aug.verdier@gmail.com>
2023-10-18 16:22:02 +0200

    Création main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+
+    from package import get_data_book as book
+    from package import get_data_book_by_categories as page
+    from package import get_data_categories as cat
+
+print(cat)
