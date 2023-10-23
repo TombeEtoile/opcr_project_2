1a7604cb683648113ac34333cf990dedf0c8c6de
augustinverdier <aug.verdier@gmail.com>
2023-10-23 17:20:59 +0200

    OK

--- package/test_recuperation_pages.py
+++ package/test_recuperation_pages.py
@@ -1,13 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
+base_url = "https://books.toscrape.com/catalogue/category/books/nonfiction_13/"
+r = requests.get(base_url)
+soup = BeautifulSoup(r.content, "html.parser")
 
-def scraping_pages():
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+}
 
-    r = requests.get("https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html")
-    soup = BeautifulSoup(r.content, "html.parser")
+next_url = []
 
-    link_page = []
+next_button = soup.find('li', class_='next')
 
-    link = soup.find_all("a")
-    print(link)
+while next_button is not None:
+    next_page_relative_url = next_button.find('a', href=True)['href']
+
+    page = requests.get(base_url + next_page_relative_url, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    next_button = soup.find('li', class_='next')
+    for href in next_button:
+        link_page = href.attrs["href"]
+        next_url.append(base_url + link_page)
+
+    while next_button is None:
+        break
+
+    print(next_url)
+
+
+
+
+
+
+"""
+def scraping_page():
+
+    next_url = []
+
+    next_button = soup.find("li", {"class": "next"})
+    for href in next_button:
+        link_page = href.attrs["href"]
+        next_url.append(url_cat + link_page)
+
+    return next_url
+
+
+print(scraping_page())
+"""
