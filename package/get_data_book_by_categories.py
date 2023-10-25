3efe15c197d90dc79ad870b36390ffee02065273
augustinverdier <aug.verdier@gmail.com>
2023-10-25 12:31:05 +0200

    ajout fonction scrap toutes les pages : fonctionnel

--- package/get_data_book_by_categories.py
+++ package/get_data_book_by_categories.py
@@ -1,20 +1,68 @@
 import requests
 from bs4 import BeautifulSoup
 
+"""
+def scraping_pages():
+    # scraper url page 2
 
-def get_all_categories_pages():
+    r = requests.get("https://books.toscrape.com/catalogue/category/books/fantasy_19/")
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    url = r.url
+    link_list = []
+
+    next_page = soup.find("li", {"class": "next"})
+
+    for href in next_page:
+        link_page = href.attrs["href"]
+        link_list.append(url + link_page)
+
+    return link_list
+    
+
+def scrap_all_next_page():
+    # scraper toutes les pages "next"
+
+    r = requests.get(scrap_next_page())
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    link_page = []
+
+    link = soup.find_all("a")
+
+    for href in link:
+        link_list = href.attrs["href"]
+        link_page.append("https://books.toscrape.com/catalogue/category/books/fantasy_19/" + link_list)
+
+    next_page = link_page[-1]
+
+    return next_page
+"""
+
+
+def scrap_pages():
+
+    r = requests.get("https://books.toscrape.com/catalogue/category/books/fantasy_19/page-1.html")
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    page_number = soup.find("li", {"class": "current"}).text.strip().split()
+    max_page_str = page_number[3]
+    max_page = int(max_page_str)
+    html = ".html"
 
     urls = []
-    page_number = 1
+    min_page = 1
 
-    for i in range(3):
-        i = f"https://books.toscrape.com/catalogue/category/books/fantasy_19/page-{page_number}.html"
-        page_number += 1
+    for i in range(max_page):
+        i = f"https://books.toscrape.com/catalogue/category/books/fantasy_19/page-{min_page}{html}"
+        min_page += 1
         urls.append(i)
+
     return urls
 
 
 def get_books_from_categories(url):
+    # scraper tous les livres d'une catégorie
 
     r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
@@ -41,10 +89,11 @@ def get_books_from_categories(url):
 
 
 def get_books_from_all_categories():
+    # appel la fonction 2 et 3 pour scraper tous les livres de toutes les pages "next"
 
     all_books_url = []
 
-    pages = get_all_categories_pages()
+    pages = scrap_pages()
     for page in pages:
         get_books_from_categories(url=page)
         for book in get_books_from_categories(url=page):
