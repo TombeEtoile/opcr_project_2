09744a02180eb396b5e9088bc83fcddad944df82
augustinverdier <aug.verdier@gmail.com>
2023-10-26 18:25:56 +0200

    premier test sur main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+from package import get_data_categories as cat, get_data_book_by_categories as page, get_data_book as book
+
 
 if __name__ == "__main__":
 
-    from package import get_data_book as book
-    from package import get_data_book_by_categories as page
-    from package import get_data_categories as cat
+    all_cat = cat.get_data_categories_fct(url="https://books.toscrape.com/")
+    for url_cat in all_cat:
+        all_product = page.get_books_from_categories(url=url_cat)
+        for url_product in all_product:
+            all_info_books = book.get_data_book(url=url_product)
 
-
-print(cat)
+            print(all_info_books)
