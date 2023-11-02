50502f2c7e3b1685f91158d9cf18aa423fd53b50
augustinverdier <aug.verdier@gmail.com>
2023-11-02 17:27:31 +0100

    manque retour à la ligne

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 from package import get_data_categories as cat, get_data_book_by_categories as page, get_data_book as book
+import panda as pd
 
 # resultat à obtenir :
 # Dict{cat1 : [{book1}, {book2}, {book3}], cat2 : [{book4}, {book5}, {book6}], etc...}
 
 if __name__ == "__main__":
 
-    # print(cat.dictionary_cat(url="https://books.toscrape.com"))
-    # print(page.dictionary_books(url_cat="https://books.toscrape.com/catalogue/category/books/fantasy_19/"))
-    # print(book.get_data_book())
+    off_books = []
+    off_pages = []
+    off_cat = ["https://books.toscrape.com/catalogue/category/books/travel_2/", "https://books.toscrape.com/catalogue/category/books/mystery_3/"]
+    list_dict_info_book = []
 
-    all_urls = []
-    all_urls_page_1 = []
-    all_book = []
+    for link in off_cat:
+        page.get_books_from_all_categories(url_all_pages=link)
+        off_pages.append(page.get_books_from_all_categories(url_all_pages=link))
+
+        for books in page.get_books_from_all_categories(url_all_pages=link):
+            book.get_data_book(url_book=books)
+            off_books.append(book.get_data_book(url_book=books))
+
+    dict_info_book = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), off_books)}
+    list_dict_info_book.append(dict_info_book)
+    dict_all = {cle: valeur for cle, valeur in zip(off_cat, list_dict_info_book)}
+
+    df =
+
+    print(dict_all)
+
+'''
+    all_page = []
+    all_books = []
 
     all_cat = cat.categories_url(url_home="https://books.toscrape.com")
-    for categories in all_cat:
-        page.scrap_page_number(url_cat=categories)
-        all_urls.append(page.scrap_page_number(url_cat=categories))
 
-    # print(all_urls)
-
-    for urls in all_urls:
-        page.get_books_from_categories(url_page_1=urls)
-        all_urls_page_1.append(page.get_books_from_categories(url_page_1=urls))
-
-    print(all_urls_page_1)
-
-"""
-    for link in all_urls_page_1:
+    for link in all_cat:
         page.get_books_from_all_categories(url_all_pages=link)
-        all_book.append(page.get_books_from_all_categories(url_all_pages=link))
-"""
 
-    # print(all_book)
+        for books in page.get_books_from_all_categories(url_all_pages=link):
+            book.get_data_book(url_book=books)
+
+            dict_info_book = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), book.get_data_book(url_book=books))}
+            dict_all = {cle: valeur for cle, valeur in zip(cat.cat_list(url_home="https://books.toscrape.com"), dict_info_book)}
+
+            chemin = r"/Users/augustinverdier/PycharmProjects/projet_2/liste_book_url.csv"
+            with open(chemin, "a") as c:
+                c.write(str(dict_all))
+'''
