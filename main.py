9239ffd2b7c0d149ab57d488808cba5d271a5b99
augustinverdier <aug.verdier@gmail.com>
2023-11-13 17:14:47 +0100

    Pas encore focntionnel

--- main.py
+++ main.py
@@ -5,16 +5,37 @@ from package import get_data_categories as cat, get_data_book_by_categories as p
 
 if __name__ == "__main__":
 
-    # user_request = [input("Quelle catégorie veux-tu scraper (penses à enlever le '/index.html') ? ")]
-    # user_request.append(input("Quelle page veux-tu scraper ? "))
-    pages_to_scrap = ["https://books.toscrape.com/catalogue/category/books/travel_2/", "https://books.toscrape.com/catalogue/category/books/fantasy_19/"]
-    all_pages = []
     all_books = []
-    list_dict_book = []
+    all_pages = []
+
+    cat.categories_url(url_home="https://books.toscrape.com")
+
+    for link in cat.categories_url(url_home="https://books.toscrape.com"):
+        page.get_books_from_all_categories(url_all_pages=link)
+        all_pages.append(page.get_books_from_all_categories(url_all_pages=link))
+
+        for books in page.get_books_from_all_categories(url_all_pages=link):
+            book.get_data_book(url_book=books)
+            all_books.append(book.get_data_book(url_book=books))
+
+    # dict_book = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), all_books)}
+    # final_dict_book = {cle: valeur for cle, valeur in zip(cat.categories_url(url_home="https://books.toscrape.com"), dict_book)}
+
+    test_dict = {cle: valeur for cle, valeur in zip(cat.categories_url(url_home="https://books.toscrape.com"), all_pages)}
+    test_dict_2 = {cle: valeur for cle, valeur in zip(cat.categories_url(url_home="https://books.toscrape.com"), {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), all_books)})}
+
+    chemin = r"/Users/augustinverdier/PycharmProjects/projet_2/liste_book_url.csv"
+    with open(chemin, "a") as c:
+        c.write(str(test_dict_2))
+
+"""
+    pages_to_scrap = ["https://books.toscrape.com/catalogue/category/books/travel_2/", "https://books.toscrape.com/catalogue/category/books/fantasy_19/"]
+    all_cat = []
+    all_pages = []
 
     # cat.categories_url(url_home="https://books.toscrape.com")
     # for link in cat.categories_url(url_home="https://books.toscrape.com"):
-    for link in pages_to_scrap:
+    # for link in pages_to_scrap:
         page.get_books_from_all_categories(url_all_pages=link)
         # all_pages.append(page.get_books_from_all_categories(url_all_pages=link))
 
@@ -23,33 +44,11 @@ if __name__ == "__main__":
             all_books.append(book.get_data_book(url_book=books))
 
     dict_book = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), all_books)}
+    dict_book_2 = book.get_data_book(url_book=pages_to_scrap)
     # list_dict_book.append(dict_book)
     # dict_all = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), lis)}
-
-    print(dict_book)
-
-    # chemin = r"/Users/augustinverdier/PycharmProjects/projet_2/liste_book_url.csv"
-    # with open(chemin, "a") as c:
-        # c.write(str(dict_all))
-
-"""
-    off_books = []
-    off_pages = []
-    off_cat_input = input("Quelle catégorie voulez_vous scraper ? ")
-    off_cat = [off_cat_input]
-
-    for link in off_cat:
-        page.get_books_from_all_categories(url_all_pages=link)
-        # off_pages.append(page.get_books_from_all_categories(url_all_pages=link))
-
-        for books in page.get_books_from_all_categories(url_all_pages=link):
-            book.get_data_book(url_book=books)
-            off_books.append(book.get_data_book(url_book=books))
-
-    # dict_info_book = {cle: valeur for cle, valeur in zip(page.get_books_from_all_categories(url_all_pages=link), off_books)}
-    # dict_info_book = {cle: valeur for cle, valeur in zip(off_book_name, off_books)}
-    # list_dict_info_book.append(dict_info_book)
-    dict_all = {cle: valeur for cle, valeur in zip(off_cat, off_books)}
-
-    print(dict_all)
+    
+    chemin = r"/Users/augustinverdier/PycharmProjects/projet_2/liste_book_url.xlsx"
+    with open(chemin, "a") as c:
+        c.write(str(dict_book_2))
 """
